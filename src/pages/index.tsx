5006cb9b85231579cd64eee0a971575ad4dd6c0f
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2022-11-16 11:03:29 +0200

    add component return type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // types
 import type { NextPage } from 'next';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (): JSX.Element => {
   return <h1>HELLO</h1>;
 };
 
